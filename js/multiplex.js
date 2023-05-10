b181afbfd83e21a3afaaba3ec2dfbb9f9bf5b167
Faendal <juanjoborrero@yahoo.com>
2023-05-10 12:45:58 -0500

    Inicialización del proyecto

---
+++ js/multiplex.js
@@ -0,0 +1,21 @@
+export class Multiplex 
+{
+    static descuento_taquillero = 0.3;
+    static descuento_platino = 0.1;
+    static descuento_oro = 0.2;
+    static precio_general = 10000;
+    static precio_preferencial = 15000;
+    static puntos_platino = 100;
+    static puntos_oro = 150;
+    static precio_por_punto_regular = 10000;
+    static precio_por_punto_platino = 5000;
+    static precio_por_punto_oro = 2000;
+    static CombosMultiplex = 
+    {
+        1: 25000,
+        2: 30000,
+        3: 40000,
+        4: 50000,
+        5: 60000
+    };
+}
