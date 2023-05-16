57ec08fb5fdca0544a7bf23828b9eadb03ba8cff
Faendal <juanjoborrero@yahoo.com>
2023-05-16 17:18:55 -0500

    Mejora multiplex

--- src/multiplex.mjs
+++ src/multiplex.mjs
@@ -1,3 +1,9 @@
+import { Cliente } from "./cliente.mjs";
+import { Funcion } from "./funcion.mjs";
+import { Pelicula } from "./pelicula.mjs";
+import { Sala } from "./sala.mjs";
+import { Taquillero } from "./taquillero.mjs";
+
 export class Multiplex 
 {
     static min_duracion = 45;
@@ -45,4 +51,62 @@ export class Multiplex
         "scifi",
         "thriller",
     ];
+
+    constructor()
+    {
+        this._l_salas = []
+        this._l_clientes = []
+        this._l_funciones = []
+        this._l_peliculas = []
+        this._l_taquilleros = []
+    }
+
+    get L_salas() { return this._l_salas; }
+
+    get L_clientes() { return this._l_clientes; }
+
+    get L_funciones() { return this._l_funciones; }
+
+    get L_peliculas() { return this._l_peliculas; }
+
+    get L_taquilleros() { return this._l_taquilleros; }
+
+    Contratar_taquillero(id, nombre, numero_contacto, usuario, contrasena)
+    {
+        try
+        {
+            trabajador = new Taquillero(id, nombre, numero_contacto, usuario, contrasena);
+            this._l_taquilleros.push(trabajador);
+            return trabajador;
+        }
+        catch (error) { throw new Error("Ocurrió un error contratando al taquillero\n" + error); }
+    }
+
+    Construir_sala(sillas_generales, sillas_preferenciales)
+    {
+        nueva_sala = new Sala(sillas_generales, sillas_preferenciales);
+        this._l_salas.push(nueva_sala);
+        return nueva_sala;
+    }
+
+    Crear_cliente(id, nombre, numero_contacto, usuario, contrasena)
+    {
+        nuevo_cliente = new Cliente(id, nombre, numero_contacto, usuario, contrasena);
+        this._l_clientes.push(nuevo_cliente);
+        return nuevo_cliente;
+    }
+
+    Crear_funcion(fecha_hora, sala, pelicula)
+    {
+        nueva_funcion = new Funcion(fecha_hora, sala, pelicula);
+        this._l_funciones.push(nueva_funcion);
+        return nueva_funcion;
+    }
+
+    Pasar_pelicula(nombre, duracion, edad_minima, genero, puntero_imagen)
+    {
+        nueva_peli = new Pelicula(nombre, duracion, edad_minima, genero, puntero_imagen);
+        this._l_peliculas.push(nueva_peli);
+        return nueva_peli;
+    }
 }
